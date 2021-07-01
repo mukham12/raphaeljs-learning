0971c7aa36a9a67be02072477d114c8d5bb07e67
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-01 07:22:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,4 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 let x = 100 * Math.cos(Raphael.rad(60))
 let y = 100 * Math.sin(Raphael.rad(60))
 
-var hexagon1 = paper.path(['M', 50, 150, 'Q', 225, 20, 400, 150])
-
-hexagon1.attr({  })
+var hexagon = paper.path(['M', 50, 150, 'Q', 225, 20, 400, 150])
