b110699cea8937b6316a334c0233d869deadf7b6
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-01 07:23:23 -0400

    Drawing arcs

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ let x = 100 * Math.cos(Raphael.rad(60))
 let y = 100 * Math.sin(Raphael.rad(60))
 
 var hexagon = paper.path(['M', 50, 150, 'Q', 225, 20, 400, 150])
+
+// Drawing arcs using RaphaelJS
