49d4b6e53f0dd4abccda366488fec46f3dbb6c1e
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-04 09:45:06 -0400

    Arc attributes

--- main.js
+++ main.js
@@ -8,3 +8,6 @@ let y = 100 * Math.sin(Raphael.rad(60))
 // Drawing arcs using RaphaelJS
 // Parameters for drawing an arc: A or a (rx, ry, x-rotation, large-arc-flag, sweep-flag, x, y)
 let arc = paper.path(['M', 100, 150, 'a', 130, 100, 0, 1, 1, 100, 300])
+
+// Setting attributes to the arc declared above
+arc.attr({ })
