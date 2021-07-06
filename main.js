1e49ae70d92bf1a02ff296730d94a5aac1de9d4a
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-06 12:17:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 let x = 100 * Math.cos(Raphael.rad(60))
 let y = 100 * Math.sin(Raphael.rad(60))
 
-// var hexagon = paper.path(['M', 50, 150, 'Q', 225, 20, 400, 150])
-
 // Drawing arcs using RaphaelJS
 // Parameters for drawing an arc: A or a (rx, ry, x-rotation, large-arc-flag, sweep-flag, x, y)
 let arc = paper.path(['M', 100, 150, 'a', 130, 100, 0, 1, 1, 100, 300])
