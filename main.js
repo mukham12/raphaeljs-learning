b51f9d5dbad2bed52117808a3d5ffca38c22b862
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-08 14:50:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 // Parameters for drawing an arc: A or a (rx, ry, x-rotation, large-arc-flag, sweep-flag, x, y)
 let arc = paper.path(['M', 100, 150, 'a', 130, 100, 0, 1, 1, 100, 300])
 
-// Setting attributes to the arc declared above
-arc.attr({stroke: 'green'})
-
 /* 
     Utility methods for working with paths
     Element.getTotalLength()
