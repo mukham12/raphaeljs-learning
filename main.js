2aba40fcf3f1d014141793a884a54a50b08467af
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-13 09:50:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,5 @@ let circle = paper.circle(100, 100, 50).attr({ fill: 'green' })
 circle.animate( { cx: 500, r: 15 }, 1000)
 
 let rectangle = paper.rect(100, 50, 25, 50)
+
+rectangle.animate({ opacity: 0.6, x: 400 })
