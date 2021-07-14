a6778bef7922669b2dc54c779fceba6e5bf9a70b
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-14 08:09:12 -0400

    Remove arc drawing guide

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-// Drawing arcs using RaphaelJS
-// Parameters for drawing an arc: A or a (rx, ry, x-rotation, large-arc-flag, sweep-flag, x, y)
-let arc = paper.path(['M', 100, 150, 'a', 130, 100, 0, 1, 1, 100, 300])
-
 /* 
     Utility methods for working with paths
     Element.getTotalLength()
