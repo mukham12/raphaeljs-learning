228b621520c0694271261f49c8672cd49810e677
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-07-15 12:20:02 -0400

    Add rectangle animation attributes

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 
 let rectangle = paper.rect(100, 50, 25, 50)
 
-rectangle.animate({ opacity: 0.6, x: 400 })
+rectangle.animate({
+    opacity: 0.6,
+    x: 400,
+    y: 100,
+    width: 100,
+    'stroke-width': 10
+}, 1000)
