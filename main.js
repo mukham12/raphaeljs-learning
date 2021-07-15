5dc1c164d1436daaa439ace8d3deb31090beaaa5
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-07-15 12:16:37 -0400

    Remove circle animation example

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-// Learning animations using Raphäel
-let circle = paper.circle(100, 100, 50).attr({ fill: 'green' })
-
-circle.animate( { cx: 500, r: 15 }, 1000)
-
 let rectangle = paper.rect(100, 50, 25, 50)
 
 rectangle.animate({ opacity: 0.6, x: 400 })
