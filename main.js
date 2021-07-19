fd26403663c6f8750e6667d8a04d748dba0bc28d
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-19 15:15:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
 // Learning easing functions for animation
+let square = paper.rect(500, 100, 50, 50).attr({ 'fill': '#09c', cursor: 'pointer' })
