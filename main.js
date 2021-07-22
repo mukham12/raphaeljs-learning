293c709da5cf31e11ae7e60ec1096496ebc5e722
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-07-22 15:17:22 -0400

    Animating transformations

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 let star = paper.path('M 100, 40 40, 210 190,90 10,90 160,210 z')
 
 let square = paper.rect(500, 100, 50, 50).attr({ 'fill': '#09c', cursor: 'pointer' })
+
+
+square.click(() => {
+    star.animate({
+        path: ['M', 100, 180, 'a', 140, 140, 0, 0, 1, 280, 0, 90, 90, 0, 0, 0, -180, 0, 50, 50, 0, 0, 0, -100, 0]
+    })
+})
