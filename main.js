dc21688f53dd9cf4727e31ab78a61be6c741b413
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-07-22 14:11:54 -0400

    Transformation animation

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-// Learning easing functions for animation
 let square = paper.rect(500, 100, 50, 50).attr({ 'fill': '#09c', cursor: 'pointer' })
