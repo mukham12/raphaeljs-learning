b707f97b439bb7dcb8ae6f705dfe90893761f73f
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-07-22 15:14:50 -0400

    Star shape

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
+let star = paper.path('M 100, 40 40, 210 190,90 10,90 160,210 z')
+
 let square = paper.rect(500, 100, 50, 50).attr({ 'fill': '#09c', cursor: 'pointer' })
