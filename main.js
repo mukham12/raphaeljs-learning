2e7987e238a37fa73dd97fdcfa82a7e217f04901
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-24 08:45:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 
 let star = paper.path('M 100, 40 40, 210 190,90 10,90 160,210 z')
 
-let square = paper.rect(500, 100, 50, 50).attr({ 'fill': '#09c', cursor: 'pointer' })
+// let square = paper.rect(500, 100, 50, 50).attr({ 'fill': '#09c', cursor: 'pointer' })
 
 // Transform one shape to another after clicking a green square
 square.click(() => {
