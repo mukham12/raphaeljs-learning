ac1181e6b7d6e2902c9b6b6802639098c978de5e
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-28 08:04:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-// Transform one shape to another after clicking a green square
 square.click(() => {
     star.animate({
         path: ['M', 100, 180, 'a', 140, 140, 0, 0, 1, 280, 0, 90, 90, 0, 0, 0, -180, 0, 50, 50, 0, 0, 0, -100, 0]
