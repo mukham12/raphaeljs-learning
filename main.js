7e19c574afc41f7dec16aae5644745135ed0aeba
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-07-29 08:47:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 square.click(() => {
     star.animate({
         path: ['M', 100, 180, 'a', 140, 140, 0, 0, 1, 280, 0, 90, 90, 0, 0, 0, -180, 0, 50, 50, 0, 0, 0, -100, 0]
-    }, 1000)
+    })
 })
