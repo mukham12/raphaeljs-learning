93bf40739bea648ced627db20db4a7db73c1bf2d
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-06 07:04:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-// Learning about Catmull-Rom curves
+// 
 let curve = paper.path(pathStart).attr({'stroke-width': 0, 'stroke-linejoin': 'round', 'fill': 'green'});
