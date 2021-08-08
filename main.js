bef682ab3a401475c302498375c264c999ced456
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-08 09:11:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
 // Learning to animate along a path
-let curve = paper.path(pathStart).attr({'stroke-width': 0, 'stroke-linejoin': 'round', 'fill': 'green'});
