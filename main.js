163d579764e2d7bcb5386d8ad0215699301a8375
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-10 08:23:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
 // Learning to animate along a path
-var path = paper.path(['M', 100, 100, 'C', 100, 0, 400, 200, 400, 100, 'S', 100, 200, 100, 100, 'z']).attr({'stroke-width': 2});
+var path = paper.path(['M', 100, 100, 'C', 100, 0, 400, 200, 400, 100, 'S', 100, 200, 100, 100, 'z']).attr({ 'stroke-width': 2 })
