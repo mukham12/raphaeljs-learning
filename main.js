8b90e7d9694c6d0ba07948b1fc1fc00b7a3a0875
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-12 16:14:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
 // Pausing and resuming animation
+
+var circle = paper.circle(0, 0, 13).attr({fill: '#09c', cursor: 'pointer'});
