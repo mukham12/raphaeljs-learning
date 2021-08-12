fec9ca62aa998178d738a174bbe4db95ad75715b
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-12 16:12:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
 // 
-var path = paper.path(['M', 100, 100, 'C', 100, 0, 400, 200, 400, 100, 'S', 100, 200, 100, 100, 'z']).attr({ 'stroke-width': 2 })
