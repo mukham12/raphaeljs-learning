e3f38a7d70a68fed9dcb7e64a37cb2a15c7de502
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-15 07:35:34 -0400

    Define a function signature

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 
 // Pausing and resuming animation
 var circle = paper.circle(0, 0, 13).attr({fill: '#09c', cursor: 'pointer'})
+
+const runloop = () => { }
