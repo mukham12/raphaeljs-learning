9194ea2d076a2f7b3c8b7c30129819e8be581dab
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-25 11:26:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,5 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 // Pausing and resuming animation
 var circle = paper.circle(0, 0, 13).attr({fill: '#09c', cursor: 'pointer'})
 
-const runloop = () => {
-    circle.animate({pathFactor: 1}, 4000, () => { })
-}
+const runloop = () => { }
 
