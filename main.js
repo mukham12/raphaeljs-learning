4b31aa2dd1f4f6d4102c61e5ed38fe0a2ced6f8d
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-29 08:30:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 
 // Learning how to work with existing SVG's
 let initialPath = 'M 197, 94.46 A 98.5, 94.5 0 1 1 167.39,26.92 L 98.5,94.46 z'
+let pac = paper.path(initialPath)
