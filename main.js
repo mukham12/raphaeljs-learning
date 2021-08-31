084d91b5871fc82b68a84e39184dad93a9ce31c2
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-08-31 17:00:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,3 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 // Learning how to work with existing SVG's
 let initialPath = 'M 197, 94.46 A 98.5, 94.5 0 1 1 167.39,26.92 L 98.5,94.46 z'
 let pac = paper.path(initialPath)
-pac.attr({fill: '#09c', 'stroke-width': 0})
