dbef282e578d066f4a6750b47ef030a679ca0e44
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-09-11 08:10:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeigh
 
 // Choropleth Maps
 // Choropleth maps are maps that show some particular area or region with an associated measurement or statistic.
+/*
+   For the purposes of this exercise we will work with an existing map of England downloaded from
+   Wikipedia.
+*/
