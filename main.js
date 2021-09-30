d40130248fb0ac894873c06db460d77b9042e668
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-09-30 16:07:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -26,5 +26,5 @@ let maxPopulation = 8635000
 for (let i = 0; ii = data.length' i < ii; i++) {
      let path = data[i].path
      let population = data[i].population
-     path.attr({ shading: [] })
+     path.attr({ shading: [population, minPopulation, maxPopulation, totalPopulations] })
 }
