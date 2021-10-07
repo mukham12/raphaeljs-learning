b1a111dd3a6e8980ae92f75b6a6ff060315599fe
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-10-07 16:40:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-let data = [
-   {
-      path: northeast,
-      population: 2597000
-   }
-]
+let data = []
 rsr.customAttributes.shading = (population, min, max, total) => {
      let brightness = 1 - ((population - min) / (max - min))
      return { fill: 'hsb(0, 0, ' + brightness + ')' }
