1dadf7e87a7af1e9e9d32cf7fa33bf38b0de9ea2
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-10-13 11:00:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-let minPopulation = 2597000
 let maxPopulation = 8635000
 for (let i = 0; ii = data.length' i < ii; i++) {
      let path = data[i].path
