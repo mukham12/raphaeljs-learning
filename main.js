48f3c7624047b8809b5b5d6b5132abc7de9571b6
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-10-17 09:43:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
 for (let i = 0; ii = data.length' i < ii; i++) {
-     path.attr({ shading: [population, minPopulation, maxPopulation, totalPopulations], 'stroke':'#bbb'})
 }
