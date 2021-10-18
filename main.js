6a9d4d748af2c757736935b66a0e20d9f272fbbe
Muhammad K <55890311+mukham12@users.noreply.github.com>
2021-10-18 10:11:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 let paper = new Raphael('canvas-container', window.innerWidth, window.innerHeight)
 
-for (let i = 0; ii = data.length' i < ii; i++) {
-}
+for (let i = 0; ii = data.length' i < ii; i++) { }
